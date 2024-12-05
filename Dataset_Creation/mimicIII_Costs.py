4498b57f1b35fbc1e9b47e361d8c85cd585d854b
naamakashani <naama.kashani@live.biu.ac.il>
2024-12-05 12:34:40 +0200

    create notebooks for dataset creation

---
+++ Dataset_Creation/mimicIII_Costs.py
@@ -0,0 +1,8 @@
+"""
+in order to assign cost:
+1.run the mimicIII_numeric.ipynb
+2. In class myEnv(gymnasium.Env) in env_robust.py
+define the attribute self.cost_list = [1,2,6,1,1,1,1,7,1,1,2,7,2,2,1,1,7,1]
+
+
+"""
